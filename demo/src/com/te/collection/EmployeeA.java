94bcdfb9c26861e5f170573de30364c3e1d876f8
akshay <kadakshay16@gmail.com>
2021-06-23 20:15:49 +0530

    added

--- demo/src/com/te/collection/EmployeeA.java
+++ demo/src/com/te/collection/EmployeeA.java
@@ -7,15 +7,14 @@ public class EmployeeA implements Comparator<EmployeeA> {
  int marks;
  
 	public static void main(String[] args) {
-		Comparator<EmployeeA> comparator = new Comparator<EmployeeA>() {
-		};
+		
 
 	}
 
 	@Override
 	public int compare(EmployeeA o1, EmployeeA o2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return o1.id-o2.id;
 	}
 
 }
