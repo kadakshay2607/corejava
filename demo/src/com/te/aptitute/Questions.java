292284223e5476d063a86d5f673461146dccb3f1
akshay <kadakshay16@gmail.com>
2021-06-25 07:46:55 +0530

    added

--- demo/src/com/te/aptitute/Questions.java
+++ demo/src/com/te/aptitute/Questions.java
@@ -6,7 +6,7 @@ public class Questions {
 	
 	String [] str = new String[20];
 	int [] a = new int [20];
-	
+	int skip = 0;
 	
 	Scanner scanner = new Scanner(System.in);
 	
@@ -17,7 +17,8 @@ public class Questions {
 					+" option=1"
 					+" option=2"
 					+" option=3"
-					+" option=4";
+					+" option=4"
+					+" Skip=5";
 			i++;
 		}
 	}
@@ -38,35 +39,41 @@ public class Questions {
 			else if (no == 4) {
 				a[j] = 1;
 			}
+			else if (no == 5) {
+				a[j] = 0;
+				skip++;
+			}
 		}
 	}
 	public void result() {
 		int count = 0;
+		
 		for (int i = 0; i < a.length; i++) {
 			if (a[i] == 1) {
 				count++;
 			}
 		}		
-		int correct =  count;
 		System.out.println("No of Correct Option is : " + count);
-		int per = correct/a.length*100;
-		System.out.println("Percentage for Test Is : " + per);
-		if ((per >= 35) && (per <= 50)) {
+		System.out.println("No of Questions Skipped :" + skip);
+		int per = count/a.length*100;
+		//System.out.println("Percentage for Test Is : " + per);
+		if ((count >= 3) && (count <= 8)) {
 			System.out.println("Performance is bad");	
 		}
-		else if ((per >= 51) && (per <= 65)) {
+		else if ((count >= 9) && (count <= 13)) {
 			System.out.println("Performance is good");
 		}
-		else if ((per>= 66) && (per <= 80)) {
+		else if ((count >= 14) && (count <= 17)) {
 			System.out.println("Performance is very good");
 		}
-		else if ((per >= 81) && (per <= 100)) {
+		else if ((count >= 18) && (count <= 20)) {
 			System.out.println("Performance is very Excellent");
 		}
-		System.out.println("....................................");
-		 if (per >= 65) {
+		else if (count >= 12) {
 			System.out.println("eligible for next round");
 		}
+		System.out.println("....................................");
+		
 	}
 
 }
